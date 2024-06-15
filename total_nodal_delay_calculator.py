909249d94ac3adfc85d46065bdaaa37d0475c462
Kevin Fox <kpf5297@psu.edu>
2024-06-15 09:06:10 -0400

    added formatting to form

--- total_nodal_delay_calculator.py
+++ total_nodal_delay_calculator.py
@@ -1,5 +1,21 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QGridLayout, QGroupBox, QMessageBox
+
+"""
+Total Nodal Delay Calculator
+Author: Kevin Fox
+Date: June 15, 2024
+
+Description:
+This script creates a GUI application to calculate the total end-to-end nodal delay in a network.
+The total nodal delay is calculated based on user inputs including packet length, transmission rates, distances,
+propagation speeds, processing delays, and queuing delays for four different nodes. The formula used is:
+
+Total Delay = sum of (Transmission Delay + Propagation Delay + Processing Delay + Queuing Delay) for all nodes
+
+Transmission Delay (dtran) = Packet Length (L) / Transmission Rate
+Propagation Delay (dprop) = Distance / Propagation Speed
+"""
 
 class NodalDelayCalculator(QWidget):
     def __init__(self):
@@ -9,137 +25,136 @@ class NodalDelayCalculator(QWidget):
 
     def initUI(self):
         self.setWindowTitle('Total Nodal Delay Calculator')
-        self.setGeometry(100, 100, 400, 600)
+        self.setGeometry(100, 100, 600, 800)
 
-        layout = QVBoxLayout()
+        main_layout = QVBoxLayout()
 
-        # Packet Length
+        # Packet Length Group
+        packet_length_group = QGroupBox("Packet Details")
+        packet_length_layout = QGridLayout()
+        packet_length_group.setLayout(packet_length_layout)
         self.packet_length_label = QLabel('Packet Length (bytes):')
         self.packet_length_input = QLineEdit()
-        layout.addWidget(self.packet_length_label)
-        layout.addWidget(self.packet_length_input)
+        packet_length_layout.addWidget(self.packet_length_label, 0, 0)
+        packet_length_layout.addWidget(self.packet_length_input, 0, 1)
+        main_layout.addWidget(packet_length_group)
 
-        # Transmission Rate 1
+        # Transmission and Propagation Group 1
+        tp_group1 = QGroupBox("Transmission and Propagation 1")
+        tp_layout1 = QGridLayout()
+        tp_group1.setLayout(tp_layout1)
         self.transmission_rate1_label = QLabel('Transmission Rate 1 (bps):')
         self.transmission_rate1_input = QLineEdit()
-        layout.addWidget(self.transmission_rate1_label)
-        layout.addWidget(self.transmission_rate1_input)
-
-        # Distance 1
         self.distance1_label = QLabel('Distance 1 (km):')
         self.distance1_input = QLineEdit()
-        layout.addWidget(self.distance1_label)
-        layout.addWidget(self.distance1_input)
-
-        # Propagation Speed 1
         self.propagation_speed1_label = QLabel('Propagation Speed 1 (m/s):')
         self.propagation_speed1_input = QLineEdit()
-        layout.addWidget(self.propagation_speed1_label)
-        layout.addWidget(self.propagation_speed1_input)
+        tp_layout1.addWidget(self.transmission_rate1_label, 0, 0)
+        tp_layout1.addWidget(self.transmission_rate1_input, 0, 1)
+        tp_layout1.addWidget(self.distance1_label, 1, 0)
+        tp_layout1.addWidget(self.distance1_input, 1, 1)
+        tp_layout1.addWidget(self.propagation_speed1_label, 2, 0)
+        tp_layout1.addWidget(self.propagation_speed1_input, 2, 1)
+        main_layout.addWidget(tp_group1)
 
-        # Transmission Rate 2
+        # Transmission and Propagation Group 2
+        tp_group2 = QGroupBox("Transmission and Propagation 2")
+        tp_layout2 = QGridLayout()
+        tp_group2.setLayout(tp_layout2)
         self.transmission_rate2_label = QLabel('Transmission Rate 2 (bps):')
         self.transmission_rate2_input = QLineEdit()
-        layout.addWidget(self.transmission_rate2_label)
-        layout.addWidget(self.transmission_rate2_input)
-
-        # Distance 2
         self.distance2_label = QLabel('Distance 2 (km):')
         self.distance2_input = QLineEdit()
-        layout.addWidget(self.distance2_label)
-        layout.addWidget(self.distance2_input)
-
-        # Propagation Speed 2
         self.propagation_speed2_label = QLabel('Propagation Speed 2 (m/s):')
         self.propagation_speed2_input = QLineEdit()
-        layout.addWidget(self.propagation_speed2_label)
-        layout.addWidget(self.propagation_speed2_input)
+        tp_layout2.addWidget(self.transmission_rate2_label, 0, 0)
+        tp_layout2.addWidget(self.transmission_rate2_input, 0, 1)
+        tp_layout2.addWidget(self.distance2_label, 1, 0)
+        tp_layout2.addWidget(self.distance2_input, 1, 1)
+        tp_layout2.addWidget(self.propagation_speed2_label, 2, 0)
+        tp_layout2.addWidget(self.propagation_speed2_input, 2, 1)
+        main_layout.addWidget(tp_group2)
 
-        # Transmission Rate 3
+        # Transmission and Propagation Group 3
+        tp_group3 = QGroupBox("Transmission and Propagation 3")
+        tp_layout3 = QGridLayout()
+        tp_group3.setLayout(tp_layout3)
         self.transmission_rate3_label = QLabel('Transmission Rate 3 (bps):')
         self.transmission_rate3_input = QLineEdit()
-        layout.addWidget(self.transmission_rate3_label)
-        layout.addWidget(self.transmission_rate3_input)
-
-        # Distance 3
         self.distance3_label = QLabel('Distance 3 (km):')
         self.distance3_input = QLineEdit()
-        layout.addWidget(self.distance3_label)
-        layout.addWidget(self.distance3_input)
-
-        # Propagation Speed 3
         self.propagation_speed3_label = QLabel('Propagation Speed 3 (m/s):')
         self.propagation_speed3_input = QLineEdit()
-        layout.addWidget(self.propagation_speed3_label)
-        layout.addWidget(self.propagation_speed3_input)
+        tp_layout3.addWidget(self.transmission_rate3_label, 0, 0)
+        tp_layout3.addWidget(self.transmission_rate3_input, 0, 1)
+        tp_layout3.addWidget(self.distance3_label, 1, 0)
+        tp_layout3.addWidget(self.distance3_input, 1, 1)
+        tp_layout3.addWidget(self.propagation_speed3_label, 2, 0)
+        tp_layout3.addWidget(self.propagation_speed3_input, 2, 1)
+        main_layout.addWidget(tp_group3)
 
-        # Transmission Rate 4
+        # Transmission and Propagation Group 4
+        tp_group4 = QGroupBox("Transmission and Propagation 4")
+        tp_layout4 = QGridLayout()
+        tp_group4.setLayout(tp_layout4)
         self.transmission_rate4_label = QLabel('Transmission Rate 4 (bps):')
         self.transmission_rate4_input = QLineEdit()
-        layout.addWidget(self.transmission_rate4_label)
-        layout.addWidget(self.transmission_rate4_input)
-
-        # Distance 4
         self.distance4_label = QLabel('Distance 4 (km):')
         self.distance4_input = QLineEdit()
-        layout.addWidget(self.distance4_label)
-        layout.addWidget(self.distance4_input)
-
-        # Propagation Speed 4
         self.propagation_speed4_label = QLabel('Propagation Speed 4 (m/s):')
         self.propagation_speed4_input = QLineEdit()
-        layout.addWidget(self.propagation_speed4_label)
-        layout.addWidget(self.propagation_speed4_input)
+        tp_layout4.addWidget(self.transmission_rate4_label, 0, 0)
+        tp_layout4.addWidget(self.transmission_rate4_input, 0, 1)
+        tp_layout4.addWidget(self.distance4_label, 1, 0)
+        tp_layout4.addWidget(self.distance4_input, 1, 1)
+        tp_layout4.addWidget(self.propagation_speed4_label, 2, 0)
+        tp_layout4.addWidget(self.propagation_speed4_input, 2, 1)
+        main_layout.addWidget(tp_group4)
 
-        # Processing Delay 1
+        # Delays Group
+        delays_group = QGroupBox("Delays")
+        delays_layout = QGridLayout()
+        delays_group.setLayout(delays_layout)
         self.processing_delay1_label = QLabel('Processing Delay 1 (sec):')
         self.processing_delay1_input = QLineEdit()
-        layout.addWidget(self.processing_delay1_label)
-        layout.addWidget(self.processing_delay1_input)
-
-        # Queuing Delay 1
         self.queuing_delay1_label = QLabel('Queuing Delay 1 (sec):')
         self.queuing_delay1_input = QLineEdit()
-        layout.addWidget(self.queuing_delay1_label)
-        layout.addWidget(self.queuing_delay1_input)
-
-        # Processing Delay 2
         self.processing_delay2_label = QLabel('Processing Delay 2 (sec):')
         self.processing_delay2_input = QLineEdit()
-        layout.addWidget(self.processing_delay2_label)
-        layout.addWidget(self.processing_delay2_input)
-
-        # Queuing Delay 2
         self.queuing_delay2_label = QLabel('Queuing Delay 2 (sec):')
         self.queuing_delay2_input = QLineEdit()
-        layout.addWidget(self.queuing_delay2_label)
-        layout.addWidget(self.queuing_delay2_input)
-
-        # Processing Delay 3
         self.processing_delay3_label = QLabel('Processing Delay 3 (sec):')
         self.processing_delay3_input = QLineEdit()
-        layout.addWidget(self.processing_delay3_label)
-        layout.addWidget(self.processing_delay3_input)
-
-        # Queuing Delay 3
         self.queuing_delay3_label = QLabel('Queuing Delay 3 (sec):')
         self.queuing_delay3_input = QLineEdit()
-        layout.addWidget(self.queuing_delay3_label)
-        layout.addWidget(self.queuing_delay3_input)
+        delays_layout.addWidget(self.processing_delay1_label, 0, 0)
+        delays_layout.addWidget(self.processing_delay1_input, 0, 1)
+        delays_layout.addWidget(self.queuing_delay1_label, 1, 0)
+        delays_layout.addWidget(self.queuing_delay1_input, 1, 1)
+        delays_layout.addWidget(self.processing_delay2_label, 2, 0)
+        delays_layout.addWidget(self.processing_delay2_input, 2, 1)
+        delays_layout.addWidget(self.queuing_delay2_label, 3, 0)
+        delays_layout.addWidget(self.queuing_delay2_input, 3, 1)
+        delays_layout.addWidget(self.processing_delay3_label, 4, 0)
+        delays_layout.addWidget(self.processing_delay3_input, 4, 1)
+        delays_layout.addWidget(self.queuing_delay3_label, 5, 0)
+        delays_layout.addWidget(self.queuing_delay3_input, 5, 1)
+        main_layout.addWidget(delays_group)
 
         # Calculate button
         self.calculate_button = QPushButton('Calculate', self)
         self.calculate_button.clicked.connect(self.calculate_delay)
-        layout.addWidget(self.calculate_button)
+        main_layout.addWidget(self.calculate_button)
 
         # Result label
         self.result_label = QLabel('Total End-to-End Delay: N/A')
-        layout.addWidget(self.result_label)
+        main_layout.addWidget(self.result_label)
 
-        self.setLayout(layout)
+        self.setLayout(main_layout)
 
     def calculate_delay(self):
         try:
+            # Retrieve and convert user inputs
             packet_length = int(self.packet_length_input.text())
             transmission_rate1 = int(self.transmission_rate1_input.text())
             distance1 = float(self.distance1_input.text())
@@ -169,7 +184,7 @@ class NodalDelayCalculator(QWidget):
             # Convert packet length to bits
             L = packet_length * 8
 
-            # Calculate delays
+            # Calculate transmission and propagation delays for each node
             dtran1 = L / transmission_rate1
             dprop1 = distance1 * 1000 / propagation_speed1
 
@@ -182,7 +197,7 @@ class NodalDelayCalculator(QWidget):
             dtran4 = L / transmission_rate4
             dprop4 = distance4 * 1000 / propagation_speed4
 
-            # Total end-to-end delay
+            # Calculate total end-to-end delay
             dend_end = (dtran1 + dprop1 + processing_delay1 + queuing_delay1 +
                         dtran2 + dprop2 + processing_delay2 + queuing_delay2 +
                         dtran3 + dprop3 + processing_delay3 + queuing_delay3 +
